a09e91c3ffeceda30d11f9c6bc4b0b0d663dcf27
React & Typescript Developer <36853339+GolfredoPerezFernandez@users.noreply.github.com>
2022-01-05 12:22:25 -0400

    first commit

---
+++ src/ts/modules/images/Images.ts
@@ -0,0 +1,21 @@
+/**
+* Images.ts
+* Copyright: Microsoft 2018
+*
+* Common interface for "images" module, which handles the fetching
+* of all static images.
+*/
+
+export interface ImageSourceBase {
+    todoLogo: string;
+    todoSmall: string;
+    caru1:string,
+    caru2:string,
+    caru3:string,
+    elipse:string,
+    logo:string
+    logo2:string
+    background:string
+    metamask: string;
+    stripe:string
+}
